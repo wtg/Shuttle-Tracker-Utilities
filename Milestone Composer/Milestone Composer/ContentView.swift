93deb3361df34c6a077cdbfd4be554689f1e1761
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2022-03-15 23:29:40 -0400

    [Milestone Composer] Create project

---
+++ Milestone Composer/Milestone Composer/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Milestone Composer
+//
+//  Created by Gabriel Jacoby-Cooper on 3/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	var body: some View {
+		Text("Hello, world!")
+			.padding()
+	}
+	
+}
+
+struct ContentViewPreviews: PreviewProvider {
+	
+	static var previews: some View {
+		ContentView()
+	}
+	
+}
---
+++ Milestone Composer/Milestone Composer/MilestoneComposerApp.swift
@@ -0,0 +1,19 @@
+//
+//  MilestoneComposerApp.swift
+//  Milestone Composer
+//
+//  Created by Gabriel Jacoby-Cooper on 3/15/22.
+//
+
+import SwiftUI
+import KeyManagement
+
+@main struct MilestoneComposerApp: App {
+	
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+	
+}
