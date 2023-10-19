6a31c80ce5861e25ac7bf0c2f3d1fb01953bf7cb
Nevin Joshy <joshyn@rpi.edu>
2023-10-19 19:03:00 -0400

    Fixed final functional errors

--- Announcement-Composer-Windows/test.py
+++ Announcement-Composer-Windows/test.py
@@ -18,33 +18,33 @@ subject = input("Enter the subject: ").strip()
 body = input("Enter the body: ").strip()
 scheduleType = input("Enter the schedule type(startOnly, endOnly, startAndEnd): ").strip()
 
-if(scheduleType == "s" or scheduleType == "b"):
+if(scheduleType == "startOnly" or scheduleType == "startAndEnd"):
     startstr = input("Enter Start Date(year-month-day-hour-minute): ")
     startarr = startstr.split("-")
     start = datetime.datetime(int(startarr[0]), int(startarr[1]), int(startarr[2]), int(startarr[3]), int(startarr[4]), 00)
-if(scheduleType == "e" or scheduleType == "b"):
+if(scheduleType == "endOnly" or scheduleType == "startAndEnd"):
     endstr = input("Enter End Date(year-month-day-hour-minute): ")
     endarr = endstr.split("-")
     end = datetime.datetime(int(endarr[0]), int(endarr[1]), int(endarr[2]), int(endarr[3]), int(endarr[4]), 00)
 
+#convert time to acceptable format
+start = start.replace(microsecond=00)
+end = end.replace(microsecond=00)
 start = start.isoformat() + "Z"
 end = end.isoformat() + "Z"
 
-interuptionLevel = input("Enter the interruption level(p for passive, a for active, t for timesensitive, c for critical): ").strip()
+interruptionLevel = input("Enter the interruption level(passive, active, timeSensitive, critical): ").strip()
 
-announcementDict = {"body":body, "subject":subject, "start":start, "end":end, "scheduleType":scheduleType, "id":str(id)}
+announcementDict = {"scheduleType":scheduleType,"subject":subject,"end":end,"interruptionLevel":interruptionLevel,"body":body,"start":start,"id":str(id)}
 
 #converting file to private key object
 privateKey = cryptography.hazmat.primitives.serialization.load_ssh_private_key(privateKeyFile, None)
-#print(privateKey)
 
 # posts announcements information to server
 def submitAnnouncement(announcementDict):
     concat = bytes(announcementDict["subject"] + announcementDict["body"], 'utf-8')
     signedKey = privateKey.sign(concat)
-    #print(signedKey)
     signature = base64.b64encode(signedKey).decode()
-    #print(signature)
     announcementDict["signature"] = signature
     r = requests.post("https://shuttletracker.app/announcements", data=json.dumps(announcementDict))
     # checks whether signature has been successfully read
