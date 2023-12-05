19579761ec17b5593e72407941162310a75bca16
Nevin Joshy <joshyn@rpi.edu>
2023-12-05 11:31:46 -0500

    added tkinter functionality

--- Announcement-Composer-Windows/announcement_GUI.py
+++ Announcement-Composer-Windows/announcement_GUI.py
@@ -113,3 +113,60 @@ def submit_announcement():
 
     post_announcement()
 
+root = tk.Tk()
+root.title("Announcement Creator")
+
+subject_label = tk.Label(root, text="Enter the subject:")
+subject_label.pack()
+subject_entry = tk.Entry(root)
+subject_entry.pack()
+
+body_label = tk.Label(root, text="Enter the body:")
+body_label.pack()
+body_entry = tk.Entry(root)
+body_entry.pack()
+
+schedule_type_label = tk.Label(root, text="Enter the schedule type(startOnly, endOnly, startAndEnd):")
+schedule_type_label.pack()
+schedule_type_entry = tk.Entry(root)
+schedule_type_entry.pack()
+
+interruption_level_label = tk.Label(root, text="Enter the interruption level(passive, active, timeSensitive, critical):")
+interruption_level_label.pack()
+interruption_level_entry = tk.Entry(root)
+interruption_level_entry.pack()
+
+start_label = tk.Label(root, text="Enter Start Date(year-month-day-hour-minute):")
+start_label.pack()
+start_entry = tk.Entry(root)
+start_entry.pack()
+
+end_label = tk.Label(root, text="Enter End Date(year-month-day-hour-minute):")
+end_label.pack()
+end_entry = tk.Entry(root)
+end_entry.pack()
+
+key_path_label = tk.Label(root, text="File path where user's private key is stored:")
+key_path_label.pack()
+key_path_entry = tk.Entry(root)
+key_path_entry.pack()
+
+generate_key_var = tk.IntVar()
+generate_key_check = tk.Checkbutton(root, text="Generate a new key", variable=generate_key_var)
+generate_key_check.pack()
+
+passphrase_label = tk.Label(root, text="Enter Password. If none, enter 'n':")
+passphrase_label.pack()
+passphrase_entry = tk.Entry(root)
+passphrase_entry.pack()
+
+server_choice_var = tk.StringVar()
+server_choice_label = tk.Label(root, text="Choose server (staging/production):")
+server_choice_label.pack()
+server_choice_entry = tk.Entry(root, textvariable=server_choice_var)
+server_choice_entry.pack()
+
+submit_button = tk.Button(root, text="Submit", command=submit_announcement)
+submit_button.pack()
+
+root.mainloop()
